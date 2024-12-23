66b7c24f2a0837bc422360819f7925b8e08f8e2e
Bruno Cascio <brunocascio@gmail.com>
2024-12-23 12:03:32 -0300

    fix ts errors

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -183,7 +183,13 @@ export const deleteMessageForMe: RequestHandler = async (req, res) => {
 		const exists = await WhatsappService.jidExists(session, jid, type);
 		if (!exists) return res.status(400).json({ error: "JID does not exists" });
 
-		const result = await session.chatModify({ clear: { messages: [message] } }, jid);
+		const result = await session.chatModify({
+			deleteForMe: {
+				deleteMedia: false,
+				key: { id: message.id, fromMe: message.fromMe },
+				timestamp: Number(message.timestamp),
+			}
+		}, jid);
 
 		res.status(200).json(result);
 	} catch (e) {
